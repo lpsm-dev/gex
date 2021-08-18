8226249e01142eea51bc49dde67e792ab1d9517e
Lucca Pessoa <luccapsm@protonmail.com>
2021-08-18 02:06:43 -0300

    fix: folder name choco package and scripts

---
+++ package/tools/chocolateyinstall.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference = "Stop";
+
+if (!$PSScriptRoot) {
+  $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
+}
+. "$PSScriptRoot\helper.ps1"
+
+$version      = "1.0.31"
+
+if (!(IsVersionAlreadyInstalled $version)) {
+  $stop_habbbo = if (Get-Process -Name Habbo -ErrorAction SilentlyContinue) { $false } else { $true }
+
+  $packageArgs  = @{
+    packageName     = $env:ChocolateyPackageName
+    softwareName    = "habbo"
+    url             = "https://images.habbo.com/habbo-native-clients/launcher/HabboLauncher-Setup-$version.exe"
+    fileType        = "exe"
+    silentArgs      = "/s"
+    validExitCodes  = @(0, 1641, 3010)
+  }
+
+  Install-ChocolateyPackage @packageArgs
+
+  if ($stop_habbbo -and (Get-Process -Name Habbo -ErrorAction SilentlyContinue)) {
+    Stop-Process -processname Habbo
+  }
+} else {
+  Write-Host "Habbo $version is already installed."
+}
